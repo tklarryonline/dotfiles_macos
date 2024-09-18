a3866d3718d6a3fdf1de7962c964337bfb96eaea
Luan Nguyen <2020169+tklarryonline@users.noreply.github.com>
2024-09-18 19:46:01 +1000

    brew-app: Install & configure direnv and libpq

--- zsh/tools.sh
+++ zsh/tools.sh
@@ -12,3 +12,14 @@ source $(brew --prefix)/opt/asdf/libexec/asdf.sh
 # Switch to use GNU Make installation from Brew
 # https://formulae.brew.sh/formula/make
 export PATH="$(brew --prefix make)/libexec/gnubin:$PATH"
+
+# Enables direnv
+# https://direnv.net/
+eval "$(direnv hook zsh)"
+
+# Enables iTerm2 shell integration
+# https://iterm2.com/documentation-shell-integration.html
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+# Enables libpq
+export PATH="$(brew --prefix libpq)/bin:$PATH"
