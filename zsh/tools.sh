ad0044166c3150def31ab70017df31bca159f71d
Luan Nguyen <2020169+tklarryonline@users.noreply.github.com>
2025-03-06 18:24:10 +1100

    chore: Update Brewfile, gitconfig, and zsh tools configuration

--- zsh/tools.sh
+++ zsh/tools.sh
@@ -1,9 +1,14 @@
+#!/bin/zsh
 # ***************************************************************
 # Terminal tooling settings for my shell environment, including:
 # - asdf
 # - GNU Make
 # ***************************************************************
 
+# asdf setup
+# https://asdf-vm.com/guide/getting-started.html
+export ASDF_DATA_DIR="$HOME/.asdf"
+export PATH="$ASDF_DATA_DIR/shims:$PATH"
 
 # GNU make setup
 # Switch to use GNU Make installation from Brew
@@ -23,3 +28,7 @@ test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell
 export PATH="$(brew --prefix libpq)/bin:$PATH"
 # Enables postgresql@12
 export PATH="$(brew --prefix postgresql@12)/bin:$PATH"
+
+# Set up GPG key
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+export GPG_TTY="$(tty)"
